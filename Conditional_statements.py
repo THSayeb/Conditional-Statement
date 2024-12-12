7865d4ecb9805377071995e551259f27864f2739
THSayeb <desktop499@gmail.com>
2024-12-12 21:07:58 +0600

    Conditional statement

---
+++ Conditional_statements.py
@@ -0,0 +1,13 @@
+print("Here, you can identify that about the state of your of given number \n First,")
+num = int(input("Enter your number "))
+
+if(num < 0):
+    print("Your number is negative")
+else:
+    print("Your number is not negative")
+
+print("also,")
+if(num > 0):
+    print("Given number is positive")
+else:
+    print("Given number is not negative")
